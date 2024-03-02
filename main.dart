aec68affd7a573e6c1fa14900f44b6c77a4dcd00
edwin phiri zm <edwinphiri77@outlook.com>
2024-03-02 17:20:32 +0200

    pushing dev files to git

---
+++ main.dart
@@ -0,0 +1,32 @@
+//void main() {
+  //Creating a Map with String keys and int values
+  /* Map<String, int> ages = {'Alice': 30,
+  'Bob': 25,
+  'Charles': 35,
+  };
+  print("ages of students: $ages");
+}
+*/
+
+//Define a string with runes
+//String runesString = "Runes in Dart: "
+
+/*String<dynamic> mixedList = [1, "Power learn project", true, "10.5"];
+print(mixedList);
+}
+*/
+/*Void main() {
+Map<String, dynamic> userData = {
+  "name": "Alice",
+  "Age": 30,
+  "IsStudent": true,
+  "Hobbies": ["reading", "hiking", "cooking"],
+  "address": {
+    "city": "Lusaka",
+    "country": "Kenya" 
+  };
+  };
+  print(userData["name"]);
+};
+
+*/
