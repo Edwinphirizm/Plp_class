79431fc850486566b7ced96d3239dcc9ef6ddad5
edwin phiri zm <edwinphiri77@outlook.com>
2024-03-20 01:30:25 +0200

    Introduction to css assignment push

---
+++ Practice/Practice.dart
@@ -0,0 +1,37 @@
+void main() {
+  // Declairing variables
+  int num1 = 10;
+  double num2 = 1.2;
+  num num3 = 55;
+  num num4 = 5.26;
+-
+//calculating the sum
+  num sum = num1 + num2 + num3 + num4;
+
+// Printing results
+  print("Num 1 is $num1");
+  print("Num 1 is $num2");
+  print("Num 1 is $num3");
+  print("Num 1 is $num4");
+  print("sum 1 is $sum");
+
+// Declairing values
+  String schoolName = "Power Learn Project";
+  String address = "Africa";
+
+  //Printing info
+  print("My school is $schoolName and the address is $address");
+
+// Bool values
+  bool isMarried = true;
+  print("Marital status: $isMarried");
+
+  //List string
+  List<String> names = ["John", "Mary", "Peter", "Edwin"];
+  print("value of names is $names");
+  print("Value of names[0] is ${names[0]}");
+  print("Value of names[1] is ${names[1]}");
+  print("Value of names[2] is ${names[2]}");
+
+  print(names);
+}
--- main.dart
+++ main.dart
@@ -1,21 +1,20 @@
-//void main() {
+void main() {
   //Creating a Map with String keys and int values
-  /* Map<String, int> ages = {'Alice': 30,
+Map<String, int> ages = {'Alice': 30,
   'Bob': 25,
   'Charles': 35,
   };
   print("ages of students: $ages");
-}
-*/
+
+
 
 //Define a string with runes
 //String runesString = "Runes in Dart: "
 
-/*String<dynamic> mixedList = [1, "Power learn project", true, "10.5"];
+string<dynamic> mixedList = [1, "Power learn project", true, "10.5"];
 print(mixedList);
-}
-*/
-/*Void main() {
+
+
 Map<String, dynamic> userData = {
   "name": "Alice",
   "Age": 30,
@@ -24,9 +23,6 @@ Map<String, dynamic> userData = {
   "address": {
     "city": "Lusaka",
     "country": "Kenya" 
-  };
-  };
+  }
   print(userData["name"]);
 };
-
-*/
