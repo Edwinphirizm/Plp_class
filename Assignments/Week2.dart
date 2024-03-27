2925503a2faa4c89df8e03db27717e28b575139e
edwin phiri zm <edwinphiri77@outlook.com>
2024-03-27 12:03:43 +0200

    edit wk2 dart

--- Assignments/Week2.dart
+++ Assignments/Week2.dart
@@ -2,24 +2,23 @@
 void main() {
   // int data type example
   int number = 42;
-  print('int: $number'); // Prints an integer value to the console
+  print('int: $number');
 
   // double data type example
   double temperature = 36.6;
-  print('double: $temperature'); // Prints a double value to the console
+  print('double: $temperature');
 
   // String data type example
   String greeting = 'Hello, Dart!';
-  print('String: $greeting'); // Prints a String value to the console
+  print('String: $greeting');
 
   // List data type example
   List<int> numberList = [1, 2, 3, 4, 5];
-  print('List: $numberList'); // Prints a List of integers to the console
+  print('List: $numberList');
 
   // Map data type example
   Map<String, dynamic> person = {'name': 'Alex', 'age': 28, 'isStudent': false};
-  print(
-      'Map: $person'); // Prints a Map with String keys and dynamic values to the console
+  print('Map: $person');
 
   // Testing the accuracy of the data types
   assert(number is int, 'Number should be an integer');
